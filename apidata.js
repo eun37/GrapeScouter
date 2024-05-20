edc55d0fa6211ec720bae4ef75877a9012ccfe68
eun37 <155618160+eun37@users.noreply.github.com>
2024-05-20 13:23:02 +0900

    Update apidata.js

--- apidata.js
+++ apidata.js
@@ -26,12 +26,17 @@ function numberToKorean(number) {
   return resultString;
 }
 
-let podo_force;
+let podo_force, mango_force;
 
 const btn = document.getElementById('charsetonbtn');
 const textbox = document.getElementById('Character_name');
 const charinfores = document.getElementById('charinfores');
 const charimgres = document.getElementById('charimgres');
+
+const btn2 = document.getElementById('charsetonbtn2');
+const textbox2 = document.getElementById('Character_name2');
+const charinfores2 = document.getElementById('charinfores2');
+const charimgres2 = document.getElementById('charimgres2');
 
 function get_podo() {
   fetch(`https://open.api.nexon.com/maplestory/v1/character/stat?ocid=c5bc6048ce6b33728fce893890bc431defe8d04e6d233bd35cf2fabdeb93fb0d`, {
@@ -48,10 +53,94 @@ function get_podo() {
     })
 }
 
+function get_mango() {
+  fetch(`https://open.api.nexon.com/maplestory/v1/character/stat?ocid=fe370aba52d36e65175261eaa087a9d2efe8d04e6d233bd35cf2fabdeb93fb0d`, {
+    method: 'GET',
+    headers: {
+      "x-nxopen-api-key": apikey
+    }
+  })
+    .then(res => res.json())
+    .then(myJson => {
+      const getmango = myJson.final_stat[42].stat_value;
+      mango_force = Number(getmango);
+      document.getElementById("mggij").innerHTML = `${numberToKorean(mango_force)}`;
+    })
+}
+
 get_podo();
+get_mango();
 
 let ocid = '';
 let J0J4 = ['', ''];
+
+function getUser2(username) {
+  let req_url_username = `https://open.api.nexon.com/maplestory/v1/id?character_name=${username}`;
+  fetch(req_url_username, {
+    method: "GET",
+    headers: {
+      "x-nxopen-api-key": apikey
+    }
+  })
+    .then(res => res.json())
+    .then(myJson => {
+      if (myJson.ocid == null || myJson.ocid == undefined) {
+        if (fun_res == 0) {
+          charinfores2.innerHTML = `조회할 수 없습니다.`;
+          fun_res++;
+        } else if (fun_res > 0) {
+          charinfores2.innerHTML = `조회할 수 없습니다.`;
+        }
+      }
+      let req_url_image = `https://open.api.nexon.com/maplestory/v1/character/basic?ocid=${myJson.ocid}`;
+      fetch(req_url_image, {
+        method: "GET",
+        headers: {
+          "x-nxopen-api-key": apikey
+        }
+      })
+        .then(res => res.json())
+        .then(myJson => {
+          if (fun_res == 0) {
+            charimgres2.src = myJson.character_image;
+            if (myJson.character_image == null || myJson.character_image == undefined) {
+              // charimgres.src = 'https://th.bing.com/th/id/R.9a981ce34f9140f665dbfc98df12eaa7?rik=vqi7Q%2bLw1N5NMA&riu=http%3a%2f%2fpostfiles7.naver.net%2f20131027_230%2fmimi5527_1382857144898o7avf_PNG%2f1382857142188_PicsArt_1382236000731.png%3ftype%3dw3&ehk=RjQURca2zi2pXUZhMCanJQRsfJlRNwMQ8DJFSlh20yM%3d&risl=&pid=ImgRaw&r=0';
+                charimgres2.src = '';
+            }
+          } else if (fun_res > 0) {
+              charimgres2.src = myJson.character_image;
+              if (myJson.character_image == null || myJson.character_image == undefined) {
+                // charimgres.src = 'https://th.bing.com/th/id/R.9a981ce34f9140f665dbfc98df12eaa7?rik=vqi7Q%2bLw1N5NMA&riu=http%3a%2f%2fpostfiles7.naver.net%2f20131027_230%2fmimi5527_1382857144898o7avf_PNG%2f1382857142188_PicsArt_1382236000731.png%3ftype%3dw3&ehk=RjQURca2zi2pXUZhMCanJQRsfJlRNwMQ8DJFSlh20yM%3d&risl=&pid=ImgRaw&r=0';
+                charimgres2.src = '';
+              }
+          }
+        })
+
+      let req_url_status = `https://open.api.nexon.com/maplestory/v1/character/stat?ocid=${myJson.ocid}`;
+      fetch(req_url_status, {
+        method: "GET",
+        headers: {
+          "x-nxopen-api-key": apikey
+        }
+      })
+        .then(res => res.json())
+        .then(myJson => {
+
+          let result = myJson.final_stat[42].stat_value / mango_force;
+          let result_kor = numberToKorean(myJson.final_stat[42].stat_value);
+          if (fun_res >= 0) {
+            charinfores2.innerHTML = `전투력 ${result_kor}<br>${(result).toFixed(1)} 망고`;
+            if (textbox2.value == '황금맛포도') {
+              charinfores2.innerHTML = `전투력 ${result_kor}<br>${(result).toFixed(1)} 망고<br>숯과자`;
+            }
+            if (textbox2.value == '원킬도적') {
+              charinfores2.innerHTML = `전투력 ${result_kor}<br>${(result).toFixed(1)} 망고<br>응가도적`;
+            }
+            fun_res++;
+          }
+        })
+    })
+}
 
 function getUser(username) {
   let req_url_username = `https://open.api.nexon.com/maplestory/v1/id?character_name=${username}`;
@@ -132,6 +221,17 @@ btn.onclick = () => {
   asdfasdf();
 }
 
+document.getElementById('asdfasdfasdf2').addEventListener('keydown', function(event) {
+  if(event.key === 'Enter') {
+    event.preventDefault();
+    asdfasdf2();
+  }
+})
+
+btn2.onclick = () => {
+  asdfasdf2();
+}
+
 function asdfasdf() {
   if (txva == textbox.value && textbox.value != '') {
     fun_res++;
@@ -148,6 +248,27 @@ function asdfasdf() {
         charinfores.innerHTML = `캐릭터명을 입력해주세요!`;
         // charimgres.src = 'https://th.bing.com/th/id/R.9a981ce34f9140f665dbfc98df12eaa7?rik=vqi7Q%2bLw1N5NMA&riu=http%3a%2f%2fpostfiles7.naver.net%2f20131027_230%2fmimi5527_1382857144898o7avf_PNG%2f1382857142188_PicsArt_1382236000731.png%3ftype%3dw3&ehk=RjQURca2zi2pXUZhMCanJQRsfJlRNwMQ8DJFSlh20yM%3d&risl=&pid=ImgRaw&r=0';
         charimgres.src = '';
+      fun_res++;
+    }
+  }
+}
+
+function asdfasdf2() {
+  if (txva == textbox2.value && textbox2.value != '') {
+    fun_res++;
+    return;
+  }
+  txva = textbox2.value;
+  if (txva != '') {
+    getUser2(txva);
+  } else {
+    if (fun_res == 0) {
+      charinfores2.innerHTML = `캐릭터명을 입력해주세요!`;
+      fun_res++;
+    } else if (fun_res > 0) {
+        charinfores2.innerHTML = `캐릭터명을 입력해주세요!`;
+        // charimgres.src = 'https://th.bing.com/th/id/R.9a981ce34f9140f665dbfc98df12eaa7?rik=vqi7Q%2bLw1N5NMA&riu=http%3a%2f%2fpostfiles7.naver.net%2f20131027_230%2fmimi5527_1382857144898o7avf_PNG%2f1382857142188_PicsArt_1382236000731.png%3ftype%3dw3&ehk=RjQURca2zi2pXUZhMCanJQRsfJlRNwMQ8DJFSlh20yM%3d&risl=&pid=ImgRaw&r=0';
+        charimgres2.src = '';
       fun_res++;
     }
   }
